abe5d0bfbd538cdc8b4b0719c506cd6349d4d956
Zordddd <dmsedrozdov@gmail.com>
2024-09-16 21:08:21 +0300

    Add files via upload

---
+++ text.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int a, b;
+    printf("input 2 numbers through the space: ");
+    scanf("%d %d", &a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
+
+
