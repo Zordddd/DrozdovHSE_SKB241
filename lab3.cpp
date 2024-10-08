1052f3511df855f6b0b9a133aacd4934d573231b
Zordddd <dmsedrozdov@gmail.com>
2024-10-08 20:07:35 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <ctime>
 #include <math.h>
+
+#define TEST1 "test1-lab3.txt"
+#define TEST2 "test2-lab3.txt"
+#define TEST3 "test3-lab3.txt"
+#define TEST4 "test4-lab3.txt"
+#define TEST5 "test5-lab3.txt"
 using namespace std;
+
+int test1_ans = 1;
+int test2_ans = 0;
+int test3_ans = 0;
+int test4_ans = 4;
+int test5_ans = 1;
+
 
 void qsortRecursive(long long int *mas, long long int size) {
     long long int i = 0;
@@ -36,36 +50,48 @@ void qsortRecursive(long long int *mas, long long int size) {
     }
 }
 
+long long int CreateTwoArrays(long long *cups1,long long *cups2, char name_file[]){
+    long long int len, num, i, sum = 0;
+
+    ifstream file;
+    file.open(name_file);
+
+    if (file.is_open()){
+
+        file >> len;
+
+        for (i = 0; i < len / 2; i++){
+            file >> num;
+            cups1[i] = num;
+            sum += num;
+        }
+
+        for (i = 0; i < len / 2 + len % 2; i++){
+            file >> num;
+            cups2[i] = num;
+            sum += num;
+        }
+
+        file.close();
+
+    }
+
+
+    return sum;
+}
 
 int main(void){
-
     int n;
-    cin >> n;
-    long long int cups1[n/2], sums1[(long long int)pow(2, n/2)], sums2[(long long int)pow(2, n/2 + n%2)];
-    long long int cups2[n/2 + n % 2];
-    long long int sum = 0;
+    char file[] = TEST5;
+    ifstream f;
+    f.open(file);
+    if (f.is_open()) f >> n;
+    f.close();
+
+
+    long long int cups1[n/2], cups2[n/2 + n % 2], sums1[(long long int)pow(2, n/2)], sums2[(long long int)pow(2, n/2 + n%2)];;
+    long long int sum = CreateTwoArrays(cups1, cups2, file);
     long long int min_diff = 1000000000000, min_diff_h = 1000000000000;
-
-
-    for (long long int i = 0; i < n / 2; i++){
-        
-        long long int num;
-        cin >> num;
-        cups1[i] = num;
-        sum += num;
-    }
-
-    for (long long int i = 0; i < (n / 2) + (n % 2); i++){
-        
-        long long int num;
-        cin >> num;
-        // cin >> num;
-        cups2[i] = num;
-        // cout << num << "\n";
-        sum += num;
-    }
-
-
     int start = clock();
 
     for (long long int mask = 0; mask < pow(2, n/2); mask++){
@@ -92,10 +118,6 @@ int main(void){
     qsortRecursive(sums1, (long long int)pow(2, n/2));
     qsortRecursive(sums2, (long long int)pow(2, n/2 + n%2));
     
-    // for (int i = 0; i < pow(2, n / 2); i++) cout << sums1[i] << " ";
-    //cout << "\n";
-    //for (int i = 0; i < pow(2, n / 2 + n % 2); i++) cout << sums2[i] << " ";
-    //cout << "\n";
     
     for (long long int i = 0; i < (long long int)pow(2, n/2); i++){
         long long int left = 0, right = (long long int)pow(2, n / 2 + n % 2) - 1, mid = (right + left) / 2;
@@ -120,7 +142,10 @@ int main(void){
     
     int finish = clock();
 
+
     cout << "answer - " <<  abs(sum - min_diff * 2) << "\n";
+    if (abs(sum - min_diff * 2) == test5_ans) cout << "Test is passed\n";
+    else cout << "Test is not passed\n";
     cout << "time - " << double(finish - start) / CLOCKS_PER_SEC << endl;
 
 
