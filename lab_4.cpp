4d4bea5d6ddd36ab435e4a16901bb7ac91068993
Zordddd <dmsedrozdov@gmail.com>
2024-10-09 19:35:28 +0300

    Update lab_4.cpp

--- lab_4.cpp
+++ lab_4.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <time.h>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
 
 void print_partition(long long n) {
     if (n % 4 == 1 || n % 4 == 2) {
@@ -13,44 +17,90 @@ void print_partition(long long n) {
     cout << "Answer: Yes\n";
     long long lenght1 = 1;
 
+    ofstream file;
 
-    cout << "First person part:\n" << current << " ";
-    for (long long int i = n - 1; i >= 1; i--){
-        if (current + i <= polusum){                
-            cout << i << " ";
-            current += i;
-            lenght1++;
-        }
+    file.open("CurrentTest.txt");
 
-        else {
-            
-            cout << polusum - current << "\n";
-            num = i;
-            flag = polusum - current;
-            lenght1++;
-            break;
+    if (file.is_open()){
+        file << "First person:\n" << current << " ";
+        for (long long int i = n - 1; i >= 1; i--){
+            if (current + i <= polusum){                
+                file << i << " ";
+                current += i;
+                lenght1++;
+            }
+
+            else {
                 
+                file << polusum - current << "\n";
+                num = i;
+                flag = polusum - current;
+                lenght1++;
+                break;
+                    
+            }
         }
+
+
+        file << "Second person:\n";
+        for (; num >= 1; num--){
+            if (num != flag) file << num << " ";
+        }
+        
+        
+        file << "\nLenght of first group: " << lenght1 << "\n";
+        file << "Lenght of second group: " << n - lenght1 << "\n";
     }
 
-
-    cout << "Second group:\n";
-    for (; num >= 1; num--){
-        if (num != flag) cout << num << " ";
-    }
-    
-    
-    cout << "\nLenght of first group: " << lenght1 << "\n";
-    cout << "Lenght of second group: " << n - lenght1 << "\n";
-
+    file.close();
 }
 
+
+void FilesEqual(char name_of_file1[], char name_of_file2[]){
+
+    ifstream file1(name_of_file1),  file2(name_of_file2);
+
+    if(file1.is_open() && file2.is_open()){
+        string str1, str2;
+
+        while (getline(file1, str1) && getline(file2, str2)){
+            int equal = str1 == str2;
+
+            if (!equal) {
+                cout << "Answer is wrong(or you input wrong number)\n";
+                return;
+            }
+        }
+    }
+    else {
+        cout << "Wrong name of files\n";
+        return;
+    }
+
+    file1.close();
+    file2.close();
+
+    cout << "Answer is right!\n";
+    
+}
+
+
 int main() {
+    char filename1[] = "CurrentTest.txt";
+    char filename2[] = "test4_lab4.txt";
     long long n;
     cin >> n;
+
     int start = clock();
+
+
     print_partition(n);
+    FilesEqual(filename1, filename2);
+
+
     int finish = clock();
     cout << "Time of process: "<< (double)(finish - start) / 1000.0 << endl;
+
+
     return 0;
 }
